90c6eedcd7993396eea7643cd36df84754d61ed1
Nic Dahlquist <nicd@stanford.edu>
2015-03-07 03:08:04 -0800

    wip

--- install_ndk.sh
+++ install_ndk.sh
@@ -12,3 +12,5 @@ fi
 curl http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin >> ndk_installer.bin
 chmod a+x ndk_installer.bin
 ./ndk_installer.bin
+
+sudo ln -s android-ndk-r10d/ndk-build /usr/bin/ndk-build
