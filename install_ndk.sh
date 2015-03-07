6172440d8335b20e7f107144e41364b6bc02caa1
Nic Dahlquist <nicd@stanford.edu>
2015-03-07 03:02:56 -0800

    wip

--- install_ndk.sh
+++ install_ndk.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # Installs the Android NDK r10d for 64-bit Linux.
 
+set -e
+
+if [ -e android-ndk-r10d ];
+then
+	echo "The NDK is already installed. Skipping installation."
+	exit 0
+fi
+
 curl http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin >> ndk_installer.bin
 chmod a+x ndk_installer.bin
 ./ndk_installer.bin
-export PATH=PATH:$(pwd)/android-ndk-r10d
